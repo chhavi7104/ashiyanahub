3109bb1e1b39cfce8dd6bb792d9cd63f8c54ace0
chhavi7104 <chhavi7104@gmail.com>
2025-02-12 00:35:33 +0530

    changed

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
 
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
+dotenv.config()
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+})
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
