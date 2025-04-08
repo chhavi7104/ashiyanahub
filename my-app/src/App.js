33f869ebba1008d8fe5a9809649f127c135de7dc
Chhavi <162900417+chhavi7104@users.noreply.github.com>
2025-04-08 16:23:24 +0530

    new-commit
    new commit

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,23 +1,17 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="navbar">
+       <ul id="navbar">
+            <li>Home</li>
+            <li>Shop</li>
+            <li>Blog</li>
+            <li>About</li>
+            <li>Contact</li>
+            
+        </ul>   
     </div>
   );
 }
